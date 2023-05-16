046ad217780d08be50374530003a62e18e8b0d35
Nicholas Cheong <nicholascheon@gmail.com>
2023-05-16 10:50:44 +0800

    Add comments and new methods

--- src/Demandable.py
+++ src/Demandable.py
@@ -9,9 +9,7 @@ class Demandable:
         self.inv_level = {}  ## Each item has multiple inv level
         self.inv_pos = {}
         self.upstream = []  ## Each upstream Demandables
-        self.holding_cost = (
-            holding_cost  ## Possibly change for each item. perhaps a multiplier?
-        )
+        self.holding_cost = holding_cost  ## Possibly change for each item. perhaps a multiplier
 
         self.backorder = 0
         self.fixed_cost = fixed_cost
@@ -20,16 +18,36 @@ class Demandable:
         self.arrivals = []
         self.s = s
         self.S = S
+        
 
-    """ def __str__ """
+    
+    
+    
 
     def change_s(self, new_s):
+        """Changes lower bound s
+
+        Args:
+            new_s (int): new s 
+        """
         self.s = new_s
 
     def change_S(self, new_S):
+        """Changes upper bound s
+
+        Args:
+            new_S (int): new S
+        """
         self.S = new_S
 
+
     def demand(self, num_demands: int, t) -> None:
+        """Request stock from upstream
+
+        Args:
+            num_demands (int): amount requested
+            t (int): time stamp
+        """
         ## For each upstream Demandable, ask for this amount
         for demandable in self.upstream:
             new_items = demandable.get_items(num_demands, t)
@@ -39,7 +57,15 @@ class Demandable:
                 # Update inv_pos with new level of items
                 self.inv_pos[item] += new_items[item]
 
-    def get_items(self, num_get: int, t):
+    def get_items(self, num_get: int):
+        """Update inv level and inv pos for upstream
+
+        Args:
+            num_get (int): amount requested from downstream
+
+        Returns:
+            items_out (int): amount that each upstream demandable can provide 
+        """
         items_out = {}
         min_item = min(min(list(self.inv_level.values())), num_get)
         curr_backorder = num_get - min_item
@@ -51,12 +77,46 @@ class Demandable:
         return items_out
 
     def add_upstream(self, demandable: "Demandable") -> None:
+        """Adds a demandable into upstream
+
+        Args:
+            demandable (Demandable): To be added in upstream
+        """
         self.upstream.append(demandable)
         # Change later, perhaps random starting inventory ISSUE here
 
-    def add_item(self, item: "Item", amt: int):
+    def add_item(self, item: "Item", amt = 0):
+        """Add item to demandable and its downstream, create inv level and inv pos
+
+        Args:
+            item (Item): Item added
+            amt (int): Amount of item to be added
+        """
         self.inv_level[item] = amt
         self.inv_pos[item] = amt
+
+    def order_cost(self, item, amt, t):
+        """Adds order and returns cost of ordering
+
+        Args:
+            item (Item): item to be ordered
+            amt (int): amount to order
+            t (int): timestamp
+
+        Returns:
+            int: total order cost
+        """
+        self.arrivals.append([t + lead_time, item, amt])
+        return amt * item.get_cost + self.fixed_cost
+
+    def update_inventory(self, t):
+        self.inv_pos = self.inv_level.copy()
+        for arrival in self.arrivals:
+            time, item, amt = arrival
+            if t == time:
+                self.inv_level[item] += amt
+                arrivals.remove(arrival)
+            self.inv_pos[item] += amt
 
     def get_hc(self) -> int:
         total = 0
