7be17540f6e501d6b83f2218eaef5e835b11bff0
darryl-chan <darrylchan8@yahoo.com.sg>
2023-05-21 17:22:48 +0800

    Debug Main

--- src/Main.py
+++ src/Main.py
@@ -6,7 +6,7 @@ def Main():
     
 
     def simulate(self, demand, t):
-        self.state = create_state()
+        self.state.create_state()
         for i in range(t):
             self.state.update_state(demand, i)
         
