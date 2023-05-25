f4946bc8821d839ebd9e3759b137704ffcf202a0
Nicholas Cheong <nicholascheon@gmail.com>
2023-05-25 10:28:49 +0800

    add tts

--- src/MLModel.py
+++ src/MLModel.py
@@ -2,6 +2,7 @@ from MLGenerateData import MLGenerateData
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
+import sklearn.model_selection
 
 data = MLGenerateData()
 data.create_data()
@@ -9,6 +10,7 @@ data.create_data()
 
 target = data.df['profit']
 predictors = data.df.drop(['profit'], axis=1)
+X_train, X_test, y_train, y_test = train_test_split(predictors, target, test_size=0.33, random_state=42)
 n_cols = predictors.shape[1]
 
 
@@ -21,7 +23,9 @@ model.add(layers.Dense(layers.Dropout(0.25)))
 model.add(layers.Dense(1, activation='relu'))
 
 model.compile(optimiser='adam', loss='mean_squared_error', metrics=['accuracy'])
-model.fit(predictors, target)
+model.fit(X_train, y_train)
+results = model.predict(X_test)
+print(model.evaluate(results, y_test))
 
 
 
