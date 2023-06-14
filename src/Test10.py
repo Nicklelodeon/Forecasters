783206387d4f4f649030ac1b84bf1f271436b9fd
darryl-chan <113418549+darryl-chan@users.noreply.github.com>
2023-06-14 10:50:39 +0800

    Edited Test10

--- src/Test10.py
+++ src/Test10.py
@@ -19,8 +19,9 @@ def validate(start_inventory, s_DC1, S_DC1, s_DC2, S_DC2, s_r1, S_r1):
     state.changeable_network[2].change_order_point(round(s_DC2), round(S_DC2))
     total_sum = 0
     np.random.seed(1234)
+    lst = np.reshape(demand.simulate_normal_no_season(periods = 24 * 100, mean=mean, std=std), (100, 24))
     for z in range(100):
-        state.set_demand_list(demand.simulate_normal_no_season(mean=mean, std=std))
+        state.set_demand_list(lst[z])
         print(state.demand_list)
         for i in range(24):
             # if (s_DC1[i] >= S_DC1[i] or s_DC2[i] >= S_DC2[i] or s_r1[i] >= S_r1[i]):
@@ -33,8 +34,10 @@ def validate(start_inventory, s_DC1, S_DC1, s_DC2, S_DC2, s_r1, S_r1):
         state.reset(start_inventory)
     return total_sum / 100
 
-# x = validate(158, 71, 182, 67, 179, 80, 134)
-x = validate(58, 31,  85,  32, 121,  50,  78)
+#x = validate(158, 71, 182, 67, 179, 80, 134)
+x = validate( 81,  49,  71,  44, 108,  41,  50)
 print(x)
 
 # [31.0, 34.0, 32.0, 32.0, 32.0, 27.0, 27.0, 21.0, 30.0, 36.0, 27.0, 35.0, 29.0, 34.0, 36.0, 28.0, 22.0, 28.0, 35.0, 25.0, 30.0, 29.0, 31.0, 25.0]
+# [16.0, 15.0, 16.0, 10.0, 16.0, 14.0, 16.0, 12.0, 12.0, 14.0, 16.0, 20.0, 13.0, 17.0, 11.0, 18.0, 15.0, 14.0, 17.0, 16.0, 16.0, 15.0, 15.0, 20.0]
+[16.0, 12.0, 18.0, 14.0, 14.0, 17.0, 17.0, 14.0, 15.0, 10.0, 18.0, 17.0, 17.0, 11.0, 14.0, 15.0, 16.0, 16.0, 18.0, 12.0, 15.0, 14.0, 16.0, 16.0]
