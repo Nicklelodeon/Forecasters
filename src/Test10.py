f3c1cdb932288e206c957a43854a8de2acdd95ce
Nicholas Cheong <nicholascheon@gmail.com>
2023-06-14 10:04:15 +0800

    updat test 10

--- src/Test10.py
+++ src/Test10.py
@@ -4,6 +4,11 @@ from BayesianState import BayesianState
 import numpy as np
 from GenerateDemandMonthly import GenerateDemandMonthly 
 from State import State
+import pandas as pd 
+
+df = pd.read_csv("./src/TOTALSA.csv")
+mean = df['TOTALSA'].mean()
+std = df['TOTALSA'].std()
 
 def validate(start_inventory, s_DC1, S_DC1, s_DC2, S_DC2, s_r1, S_r1):
     demand = GenerateDemandMonthly()
@@ -13,8 +18,9 @@ def validate(start_inventory, s_DC1, S_DC1, s_DC2, S_DC2, s_r1, S_r1):
     state.changeable_network[1].change_order_point(round(s_DC1), round(S_DC1))
     state.changeable_network[2].change_order_point(round(s_DC2), round(S_DC2))
     total_sum = 0
+    np.random.seed(1234)
     for z in range(100):
-        state.set_demand_list(demand.simulate_normal_no_season())
+        state.set_demand_list(demand.simulate_normal_no_season(mean=mean, std=std))
         print(state.demand_list)
         for i in range(24):
             # if (s_DC1[i] >= S_DC1[i] or s_DC2[i] >= S_DC2[i] or s_r1[i] >= S_r1[i]):
@@ -28,7 +34,7 @@ def validate(start_inventory, s_DC1, S_DC1, s_DC2, S_DC2, s_r1, S_r1):
     return total_sum / 100
 
 # x = validate(158, 71, 182, 67, 179, 80, 134)
-x = validate(122, 78, 185, 57, 152, 78, 182)
+x = validate(58, 31,  85,  32, 121,  50,  78)
 print(x)
 
 # [31.0, 34.0, 32.0, 32.0, 32.0, 27.0, 27.0, 21.0, 30.0, 36.0, 27.0, 35.0, 29.0, 34.0, 36.0, 28.0, 22.0, 28.0, 35.0, 25.0, 30.0, 29.0, 31.0, 25.0]
--- src/Test_Bayesian_Optimisation.py
+++ src/Test_Bayesian_Optimisation.py
@@ -1,21 +1,21 @@
-from BayesianOptimisation import BayesianOptimisation
-from bayes_opt import BayesianOptimization
-import random
+# from BayesianOptimisation import BayesianOptimisation
+# from bayes_opt import BayesianOptimization
+# import random
 
-pbounds = {'start_inventory': (120, 200), 's_DC1': (20, 80), 'S_DC1': (120, 200), 's_DC2': (20, 80), 'S_DC2': (120, 200), 's_r1':(20, 80), 'S_r1': (120, 200)}
-optimizer = BayesianOptimization(
-    f=BayesianOptimisation,
-    pbounds=pbounds,
-    random_state=0,
-    allow_duplicate_points=True
-)
+# pbounds = {'start_inventory': (120, 200), 's_DC1': (20, 80), 'S_DC1': (120, 200), 's_DC2': (20, 80), 'S_DC2': (120, 200), 's_r1':(20, 80), 'S_r1': (120, 200)}
+# optimizer = BayesianOptimization(
+#     f=BayesianOptimisation,
+#     pbounds=pbounds,
+#     random_state=0,
+#     allow_duplicate_points=True
+# )
 
-optimizer.maximize(
-    init_points = 100,
-    n_iter=1000
-)
+# optimizer.maximize(
+#     init_points = 100,
+#     n_iter=1000
+# )
 
-print(optimizer.max)
+# print(optimizer.max)
 
 # {'target': 85380.59333239481, 'params': {'S_DC1': 140.84650302538822, 'S_DC2': 154.50005745106537, 'S_r1': 180.0, 's_DC1': 31.435088094844822, 's_DC2': 60.0, 's_r1': 60.0, 'start_inventory': 147.88383597910882}}
 
@@ -24,169 +24,174 @@ print(optimizer.max)
 
 
 
-# # example of bayesian optimization for a 1d function from scratch
-# import math
-# from math import sin
-# from math import pi
-# from numpy import arange
-# from numpy import vstack
-# from numpy import argmax
-# from numpy import asarray
-# from numpy.random import normal
-# from scipy.stats import norm
-# from sklearn.gaussian_process import GaussianProcessRegressor
-# from warnings import catch_warnings
-# from warnings import simplefilter
-# from matplotlib import pyplot
-# from GenerateDemandMonthly import GenerateDemandMonthly
-# import random
-# import numpy as np 
-# from BayesianState import BayesianState
-# from sklearn.gaussian_process.kernels import RBF, ConstantKernel as C
-# from sklearn.gaussian_process.kernels import Matern
+# example of bayesian optimization for a 1d function from scratch
+import math
+from math import sin
+from math import pi
+from numpy import arange
+from numpy import vstack
+from numpy import argmax
+from numpy import asarray
+from numpy.random import normal
+from scipy.stats import norm
+from sklearn.gaussian_process import GaussianProcessRegressor
+from warnings import catch_warnings
+from warnings import simplefilter
+from matplotlib import pyplot
+from GenerateDemandMonthly import GenerateDemandMonthly
+import random
+import numpy as np 
+from BayesianState import BayesianState
+from sklearn.gaussian_process.kernels import RBF, ConstantKernel as C
+from sklearn.gaussian_process.kernels import Matern
+import pandas as pd 
+from State import State
 
+df = pd.read_csv("./src/TOTALSA.csv")
+mean = df['TOTALSA'].mean()
+std = df['TOTALSA'].std()
  
-# # objective function
-# def objective(lst):
-#     n = len(lst) // 6
-#     s_DC1 = lst[:n]
-#     S_DC1 = lst[n:2*n]
-#     s_DC2 = lst[2*n:3*n]
-#     S_DC2 = lst[3*n:4*n]
-#     s_r1 = lst[4*n:5*n] 
-#     S_r1 = lst[5*n:]
-#     demand = GenerateDemandMonthly()
-#     state = BayesianState()
-#     state.create_state([-1 ,0, 1, 1, 2, 2])
-#     total_sum = 0
-#     for z in range(30):
-#         state.set_demand_list(demand.simulate_normal(1))
-#         for i in range(12):
-#             if (s_DC1[i] >= S_DC1[i] or s_DC2[i] >= S_DC2[i] or s_r1[i] >= S_r1[i]):
-#                     return -100000
-#             state.changeable_network[0].change_order_point(round(s_r1[i]), round(S_r1[i]))
-#             state.changeable_network[1].change_order_point(round(s_DC1[i]), round(S_DC1[i]))
-#             state.changeable_network[2].change_order_point(round(s_DC2[i]), round(S_DC2[i]))
-#             state.update_state(i)
-#         total_sum += state.rewards
-#         state.reset()
-#     return total_sum / 30
+# objective function
+def objective(lst):
+    s_DC1, S_DC1, s_DC2, S_DC2, s_r1, S_r1, start_inventory = lst
+    # n = len(lst) // 6
+    # s_DC1 = lst[:n]
+    # S_DC1 = lst[n:2*n]
+    # s_DC2 = lst[2*n:3*n]
+    # S_DC2 = lst[3*n:4*n]
+    # s_r1 = lst[4*n:5*n] 
+    # S_r1 = lst[5*n:]
+    if (s_DC1 >= S_DC1 or s_DC2 >= S_DC2 or s_r1 >= S_r1):
+        return -100000
+    demand = GenerateDemandMonthly()
+    state = State()
+    state.create_state([-1 ,0, 1, 1, 2, 2], amount=start_inventory)
+    state.changeable_network[0].change_order_point(round(s_r1), round(S_r1))
+    state.changeable_network[1].change_order_point(round(s_DC1), round(S_DC1))
+    state.changeable_network[2].change_order_point(round(s_DC2), round(S_DC2))
+    total_sum = 0
+    np.random.seed(1234)
+    for z in range(100):
+        state.set_demand_list(demand.simulate_normal_no_season(mean = mean, std = std))
+        for i in range(24):
+            # if (s_DC1[i] >= S_DC1[i] or s_DC2[i] >= S_DC2[i] or s_r1[i] >= S_r1[i]):
+            #         return -100000
+            # state.changeable_network[0].change_order_point(round(s_r1[i]), round(S_r1[i]))
+            # state.changeable_network[1].change_order_point(round(s_DC1[i]), round(S_DC1[i]))
+            # state.changeable_network[2].change_order_point(round(s_DC2[i]), round(S_DC2[i]))
+            state.update_state(i)
+        total_sum += state.rewards
+        state.reset(start_inventory)
+    return total_sum / 100
  
-# # surrogate or approximation for the objective function
-# def surrogate(model, X):
-#  # catch any warning generated when making a prediction
-#  with catch_warnings():
-#  # ignore generated warnings
-#     simplefilter("ignore")
-#  return model.predict(X.reshape(1, -1), return_std=True)
+# surrogate or approximation for the objective function
+def surrogate(model, X):
+ # catch any warning generated when making a prediction
+ with catch_warnings():
+ # ignore generated warnings
+    simplefilter("ignore")
+ return model.predict(X.reshape(1, -1), return_std=True)
  
-# # probability of improvement acquisition function
-# def acquisition(X, Xsamples, model):
-#  # calculate the best surrogate score found so far
-#     best = 0
-#     for i in range(len(X)):
-#         yhat, _ = surrogate(model, X[i])
-#         best = max(yhat, best)
-#  # calculate mean and stdev via surrogate function
-#     best_mu = 0
-#     best_std = 0
-#     for i in range(len(Xsamples)):
-#         mu, std = surrogate(model, Xsamples[i])
-#         mu = max(mu, best_mu)
-#         std = max(std, best_std)
-#     #  mu = mu[:, 0]
-#     # calculate the probability of improvement
-#     probs = norm.cdf((best_mu - best) / (best_std+1E-9))
-#     return probs
+# probability of improvement acquisition function
+def acquisition(X, Xsamples, model):
+ # calculate the best surrogate score found so far
+    best = 0
+    for i in range(len(X)):
+        yhat, _ = surrogate(model, X[i])
+        best = max(yhat, best)
+ # calculate mean and stdev via surrogate function
+    best_mu = 0
+    best_std = 0
+    for i in range(len(Xsamples)):
+        mu, std = surrogate(model, Xsamples[i])
+        mu = max(mu, best_mu)
+        std = max(std, best_std)
+    #  mu = mu[:, 0]
+    # calculate the probability of improvement
+    probs = norm.cdf((best_mu - best) / (best_std+1E-9))
+    return probs
  
-# # optimize the acquisition function
-# def opt_acquisition(X, y, model):
-#  # random search, generate random samples
-#     Xsamples = []
+# optimize the acquisition function
+def opt_acquisition(X, y, model):
+ # random search, generate random samples
+    Xsamples = []
 
-#     for x in range(100):
-#         z = []
-#         for y in range(3):
-#             z.extend(random.choices(range(20, 50), k=12))
-#             z.extend(random.choices(range(60, 90), k=12))
-#         Xsamples.append(z)
-#     Xsamples = asarray(Xsamples)
-#     #  Xsamples = Xsamples.reshape(1200, 6)
-#     # calculate the acquisition function for each sample
-#     scores = acquisition(X, Xsamples, model)
-#     # locate the index of the largest scores
-#     ix = argmax(scores)
-#     return Xsamples[ix]
+    for x in range(100):
+        Xsamples.append([random.randint(round(mean * 2), round(mean * 4)), random.randint(round(mean * 5), round(mean * 8)), random.randint(round(mean * 2), round(mean * 4)), random.randint(round(mean * 5), round(mean * 8)), random.randint(round(mean * 2), round(mean * 4)), random.randint(round(mean * 5), round(mean * 8)), random.randint(round(mean * 2), round(mean * 8))])
+    Xsamples = asarray(Xsamples)
+    #  Xsamples = Xsamples.reshape(1200, 6)
+    # calculate the acquisition function for each sample
+    scores = acquisition(X, Xsamples, model)
+    # locate the index of the largest scores
+    ix = argmax(scores)
+    return Xsamples[ix]
  
-# # # plot real observations vs surrogate function
-# # def plot(X, y, model):
-# #  # scatter plot of inputs and real objective function
-# #  pyplot.scatter(X, y)
-# #  # line plot of surrogate function across domain
-# #  Xsamples = asarray(arange(0, 1, 0.001))
-# #  Xsamples = Xsamples.reshape(len(Xsamples), 1)
-# #  ysamples, _ = surrogate(model, Xsamples)
-# #  pyplot.plot(Xsamples, ysamples)
-# #  # show the plot
-# #  pyplot.show()
+# # plot real observations vs surrogate function
+# def plot(X, y, model):
+#  # scatter plot of inputs and real objective function
+#  pyplot.scatter(X, y)
+#  # line plot of surrogate function across domain
+#  Xsamples = asarray(arange(0, 1, 0.001))
+#  Xsamples = Xsamples.reshape(len(Xsamples), 1)
+#  ysamples, _ = surrogate(model, Xsamples)
+#  pyplot.plot(Xsamples, ysamples)
+#  # show the plot
+#  pyplot.show()
  
-# # sample the domain sparsely with noise
-# X = []
-# for x in range(100):
-#     z = []
-#     for y in range(3):
-#         z.extend(random.choices(range(20, 50), k=12))
-#         z.extend(random.choices(range(60, 90), k=12))
-#     X.append((z))
-# X = asarray(X)
-# y = asarray([objective(lst) for lst in X])
-# # reshape into rows and cols
-# # X = X.reshape(100, 72)
-# y = y.reshape(-1, 1)
-# # define the model
-# kernel = Matern()
-# model = GaussianProcessRegressor(kernel=kernel, n_restarts_optimizer=10, normalize_y=True)
-# # fit the model
-# for i in range(len(y)):
-#     model.fit(X[i].reshape(1, -1), y[i])
-# # plot before hand
-# # plot(X, y, model)
-# # perform the optimization process
-# mse = 0
-# mae = 0
-# mape = 0
-# for i in range(100):
-#  # select the next point to sample
-#  x = opt_acquisition(X, y, model)
-#  # sample the point
-#  actual = objective(x)
-#  # summarize the finding
-#  print(x[0])
+# sample the domain sparsely with noise
+X = []
+for x in range(100):
+    X.append([random.randint(round(mean * 2), round(mean * 4)), random.randint(round(mean * 5), round(mean * 8)), random.randint(round(mean * 2), round(mean * 4)), random.randint(round(mean * 5), round(mean * 8)), random.randint(round(mean * 2), round(mean * 4)), random.randint(round(mean * 5), round(mean * 8)), random.randint(round(mean * 2), round(mean * 8))])
+X = asarray(X)
+y = asarray([objective(lst) for lst in X])
+# reshape into rows and cols
+# X = X.reshape(100, 72)
+y = y.reshape(-1, 1)
+# define the model
+kernel = Matern()
+model = GaussianProcessRegressor(kernel=kernel, n_restarts_optimizer=10, normalize_y=True)
+# fit the model
+for i in range(len(y)):
+    model.fit(X[i].reshape(1, -1), y[i])
+# plot before hand
+# plot(X, y, model)
+# perform the optimization process
+mse = 0
+mae = 0
+mape = 0
+for i in range(100):
+ # select the next point to sample
+ x = opt_acquisition(X, y, model)
+ # sample the point
+ actual = objective(x)
+ # summarize the finding
+ print(x[0])
 
-#  est, _ = surrogate(model, x.reshape(1, -1))
-#  print('>x=%s, f()=%3f, actual=%.3f' % (str(x), est[0], actual))
-#  mse += (actual - est) ** 2
-#  mae += abs(actual - est)
-#  mape += abs(actual - est) / actual
-#  # add the data to the dataset
-#  X = vstack((X, [x]))
-#  y = vstack((y, [actual]))
-#  # update the model
-#  model.fit(X, y)
+ est, _ = surrogate(model, x.reshape(1, -1))
+ print('>x=%s, f()=%3f, actual=%.3f' % (str(x), est[0], actual))
+ mse += (actual - est) ** 2
+ mae += abs(actual - est)
+ mape += abs(actual - est) / actual
+ # add the data to the dataset
+ X = vstack((X, [x]))
+ y = vstack((y, [actual]))
+ # update the model
+ model.fit(X, y)
 
-# mse /= 100
-# mae /= 100
-# mape /= 100
+mse /= 100
+mae /= 100
+mape /= 100
  
 
-# # plot all samples and the final surrogate function
-# # plot(X, y, model)
-# # best result
-# ix = argmax(y)
-# print('Best Result: x=%s, y=%.3f' % (str(X[ix]), y[ix]))
+# plot all samples and the final surrogate function
+# plot(X, y, model)
+# best result
+ix = argmax(y)
+print('Best Result: x=%s, y=%.3f' % (str(X[ix]), y[ix]))
 # print("mse: " + str(mse))
 # print("mae: " + str(mae))
 # print("mape: " + str(mape))
+
 
 
 # # Best Result: x=[25 48 33 30 40 33 49 37 44 41 30 32 60 63 65 63 63 87 69 78 88 61 71 74
