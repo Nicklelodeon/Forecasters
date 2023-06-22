26b14f45d446053268004a478609fb9c77e08752
Nicholas Cheong <nicholascheon@gmail.com>
2023-06-22 16:00:44 +0800

    run ipynbs

--- src/Validation.py
+++ src/Validation.py
@@ -13,10 +13,10 @@ def validate(start_inventory, s_DC1, S_DC1, s_DC2, S_DC2, s_r1, S_r1):
     mean = df['TOTALSA'].mean()
     std = df['TOTALSA'].std()
     state = State()
-    state.create_state([-1 ,0, 1, 1, 2, 2], amount=start_inventory, mean=mean, std=std, cost=4)
+    state.create_state([-1 ,0, 1, 1, 2, 2], amount=start_inventory, mean=mean, std=std)
     return (state.run(start_inventory, s_DC1, S_DC1, s_DC2, S_DC2, s_r1, S_r1))
     
 
 #x = validate(158, 71, 182, 67, 179, 80, 134)
-x = validate(86, 55, 64, 86, 126, 53, 58)
+x = validate(94, 51, 52, 44, 58, 45, 47)
 print(x)
