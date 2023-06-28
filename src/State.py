91f6905ee1fc5f72f3ce2d736f56112ee53e4519
darryl-chan <113418549+darryl-chan@users.noreply.github.com>
2023-06-28 15:56:19 +0800

    Added Test 24 period no season

--- src/State.py
+++ src/State.py
@@ -157,7 +157,30 @@ class State:
         # print("mean:", np.mean(lst))
         # print("std:", np.std(lst, ddof=1)/(self.iterations)**0.5)
         return lst
-                    
+    
+    def test_no_season_24_period(self, s_DC1, S_DC1, s_DC2, S_DC2, s_r1, S_r1):
+        period = 24
+        self.changeable_network[0].change_order_point(round(s_r1), round(S_r1))
+        self.changeable_network[1].change_order_point(round(s_DC1), round(S_DC1))
+        self.changeable_network[2].change_order_point(round(s_DC2), round(S_DC2))
+        np.random.seed(1357) # set same demand matrix
+        self.demand_matrix = np.reshape(self.demand_generator.simulate_normal_no_season(\
+            periods = period * self.iterations, mean=self.mean, std=self.std),\
+                (self.iterations, period))
+        lst = []
+        total_sum = 0
+        np.random.seed(7531)
+        for z in range(self.iterations):
+            self.reset(self.start_inventory)
+            self.set_demand_list(self.demand_matrix[z])
+            for i in range(period):
+                self.update_state(i)
+            lst.append(self.calculate_profits())
+            total_sum += self.calculate_profits()
+        
+        # print("mean:", np.mean(lst))
+        # print("std:", np.std(lst, ddof=1)/(self.iterations)**0.5)
+        return lst
     def show_network(self):
         """Creates a tree graph of the supply chain system
         """
