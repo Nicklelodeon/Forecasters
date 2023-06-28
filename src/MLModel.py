803c83975a57ddc6f575a8086e162edd2084c620
Nicholas Cheong <nicholascheon@gmail.com>
2023-06-28 14:53:02 +0800

    update

--- src/MLModel.py
+++ src/MLModel.py
@@ -3,12 +3,52 @@ import pandas as pd
 import numpy as np 
 from State import State
 import random
+import copy
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import tqdm
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from torchmetrics import MeanAbsolutePercentageError
+
+data = pd.read_csv("./src/6_24months_US_car_data.csv")
+# data = pd.read_csv("./6_24months_mldata.csv")
+# data = pd.read_csv("./72_onlinemldata.csv")
+# data = pd.read_csv("./144_mldata.csv.csv")
+
+
+#code adapted from https://machinelearningmastery.com/building-a-regression-model-in-pytorch/
+target_df = data['profit']
+target = torch.tensor(target_df)
+predictors = torch.tensor(data.drop(['profit'], axis=1).to_numpy(dtype=np.float64))
+X_train_raw, X_test_raw, y_train, y_test = train_test_split(predictors, target, test_size=0.33, random_state=42)
+scaler = StandardScaler()
+scaler.fit(X_train_raw)
+X_train = scaler.transform(X_train_raw)
+X_test = scaler.transform(X_test_raw)
+n_cols = predictors.shape[1]
 
 df = pd.read_csv("./src/TOTALSA.csv")
 mean = df['TOTALSA'].mean()
 std = df['TOTALSA'].std()
 
-model = TheModelClass(*args, **kwargs)
+model = nn.Sequential(
+    nn.Linear(n_cols, 800),
+    nn.ReLU(),
+    nn.Linear(800, 400),
+    nn.ReLU(),
+    nn.Linear(400, 200),
+    nn.ReLU(),
+    nn.Linear(200, 100),
+    nn.ReLU(),
+    nn.Linear(100, 50),
+    nn.ReLU(),
+    nn.Linear(50, 1)
+)
 model.load_state_dict(torch.load("src/DNNmodel.pt"))
 model.eval()
 
@@ -25,15 +65,15 @@ def test(model, mean, std, tests):
         if s_DC1 >= S_DC1 or s_DC2 >= S_DC2 or s_r1 >= S_r1:
             continue
 
-        params = [1, mean, std, s_DC1, S_DC1, s_DC2, S_DC2, s_r1, S_r1]
-        preds = model.predict(params)
+        params = torch.tensor([1, mean, std, s_DC1, S_DC1, s_DC2, S_DC2, s_r1, S_r1])
+        preds = model(params)
         if preds > curr_max:
             curr_max = preds
             lst = [s_DC1, S_DC1, s_DC2, S_DC2, s_r1, S_r1]
     for x in tests:
         params = [1, mean, std]
         params.extend(x)
-        preds = model.predict(params)
+        preds = model(torch.tensor(params))
         if preds > curr_max:
             curr_max = preds
             lst = x
