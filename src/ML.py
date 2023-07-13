927105685da4c780d233ef4d123658897ca6f200
Nicholas Cheong <nicholascheon@gmail.com>
2023-07-13 09:16:49 +0800

    transfer ipynb to py

---
+++ src/ML.py
@@ -0,0 +1,106 @@
+import copy
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import tqdm
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from torchmetrics import MeanAbsolutePercentageError
+ 
+data = pd.read_csv("./6_24months_US_car_data.csv")
+
+
+
+#code adapted from https://machinelearningmastery.com/building-a-regression-model-in-pytorch/
+target_df = data['profit']
+target = torch.tensor(target_df)
+predictors = torch.tensor(data.drop(['profit'], axis=1).to_numpy(dtype=np.float64))
+X_train_raw, X_test_raw, y_train, y_test = train_test_split(predictors, target, test_size=0.33, random_state=42)
+scaler = StandardScaler()
+scaler.fit(X_train_raw)
+X_train = scaler.transform(X_train_raw)
+X_test = scaler.transform(X_test_raw)
+n_cols = predictors.shape[1]
+ 
+
+# Convert to 2D PyTorch tensors
+X_train = torch.tensor(X_train, dtype=torch.float32)
+y_train = torch.tensor(y_train, dtype=torch.float32).reshape(-1, 1)
+X_test = torch.tensor(X_test, dtype=torch.float32)
+y_test = torch.tensor(y_test, dtype=torch.float32).reshape(-1, 1)
+ 
+# Define the model
+model = nn.Sequential(
+    nn.Linear(n_cols, 800),
+    nn.ReLU(),
+    nn.Linear(800, 400),
+    nn.ReLU(),
+    nn.Linear(400, 200),
+    nn.ReLU(),
+    nn.Linear(200, 100),
+    nn.ReLU(),
+    nn.Linear(100, 50),
+    nn.ReLU(),
+    nn.Linear(50, 25),
+    nn.ReLU(),
+    nn.Linear(25, 1)
+)
+
+loss_fn = nn.MSELoss()
+optimizer = optim.Adam(model.parameters(), lr=0.001)
+ 
+n_epochs = 1500  # number of epochs to run
+batch_size = 10  # size of each batch
+batch_start = torch.arange(0, len(X_train), batch_size)
+ 
+# Hold the best model
+best_mse = np.inf   
+best_weights = None
+history = []
+predictions = []
+ 
+for epoch in range(n_epochs):
+    print(epoch)
+    model.train()
+    with tqdm.tqdm(batch_start, unit="batch", mininterval=0, disable=True) as bar:
+        bar.set_description(f"Epoch {epoch}")
+        for start in bar:
+            # take a batch
+            X_batch = X_train[start:start+batch_size]
+            y_batch = y_train[start:start+batch_size]
+            # forward pass
+            y_pred = model(X_batch)
+            loss = loss_fn(y_pred, y_batch)
+            # backward pass
+            optimizer.zero_grad()
+            loss.backward()
+            # update weights
+            optimizer.step()
+            # print progress
+            bar.set_postfix(mse=float(loss))
+    # evaluate accuracy at end of each epoch
+    model.eval()
+    y_pred = model(X_test)
+    predictions.append(np.mean(y_pred.detach().numpy()))
+    mse = loss_fn(y_pred, y_test)
+    mse = float(mse)
+    history.append(mse)
+    if mse < best_mse:
+        best_mse = mse
+        best_weights = copy.deepcopy(model.state_dict())
+
+# restore model and return best accuracy
+model.load_state_dict(best_weights)
+torch.save(model.state_dict(), "/Users/nicholas/Documents/Misc/internship A*STAR/Work/model_mse.pt")
+new_pred = model(X_test)
+print("MSE: %.2f" % best_mse)
+print("RMSE: %.2f" % np.sqrt(best_mse))
+plt.plot(history)
+plt.xlabel('Number of iterations')
+plt.ylabel('MSE')
+plt.title('MSE against time')
+
+plt.show()
